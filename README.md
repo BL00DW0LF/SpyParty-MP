26cde2a67df8277b7114058618fda019e7449381
BL00DW0LF <kaleb.fowler6@gmail.com>
2014-06-02 21:11:12 -0500

    mostly done, finished history list, added the score/history tracking.  updated ToDo

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 SpyParty-M=P
 ============
 This is a quick and simple Android Application for the Missions=Points variant of SpyParty
+
+ToDo:
+Finish handling what to do when removing an error in history
+    -update score (partially done)
+    -figure out who's spy (only allowing removal of most recent entry, makes all this easier)
+    -figure out if we switch next round
+Need the ability to reset
+    -re-enable the two "player name" boxes and "first spy" toggle
--- app/src/main/java/com/gmail/kalebfowler6/DecisionOverloadFree/spypartymp/app/ActivityMain.java
+++ app/src/main/java/com/gmail/kalebfowler6/DecisionOverloadFree/spypartymp/app/ActivityMain.java
@@ -11,9 +11,12 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Spinner;
+import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -21,8 +24,10 @@ public class ActivityMain extends ActionBarActivity {
 
     private Boolean currentSpyIsMe=false;//Who is the first spy?  (gets changed to true in onCreate() )
     private Boolean switchThisRound=true;//whether spy/snipers switch in this pair
+    private Boolean editMode=true;//if we are not in a game, we are in editMode
     private List<String> scoreHistory;
     private ArrayAdapter adapterList;//want this globally, so it never disappears
+    private int totalScore=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,7 +36,7 @@ public class ActivityMain extends ActionBarActivity {
 
         fillSpinner();
         changeFirstSpy(this.getWindow().getDecorView());//set the text of first spy button
-        //prepareScoreHistory();
+        prepareScoreHistory();
     }
 
 
@@ -58,7 +63,6 @@ public class ActivityMain extends ActionBarActivity {
         if(currentSpyIsMe){
             ((Button)findViewById(R.id.buttonSpy)).setText("First Spy\n----->");
             currentSpyIsMe=false;
-
         }
         else
         {
@@ -90,15 +94,18 @@ public class ActivityMain extends ActionBarActivity {
         Spinner spinnerYourPoints = (Spinner) findViewById(R.id.spinnerYourPoints);
         if(currentSpyIsMe){
             spinnerMyPoints.setVisibility(View.VISIBLE);
+            spinnerMyPoints.setSelection(0);
             spinnerYourPoints.setVisibility(View.INVISIBLE);
         }
         else{
             spinnerMyPoints.setVisibility(View.INVISIBLE);
             spinnerYourPoints.setVisibility(View.VISIBLE);
+            spinnerYourPoints.setSelection(0);
         }
     }
 
     public void prepareScoreHistory(){//set up the listView with variable scoreHistory, and the ability to remove elements by tapping them
+        scoreHistory=new ArrayList<String>();
         final ListView listView = (ListView) findViewById(R.id.listView);
         adapterList = new ArrayAdapter(this,android.R.layout.simple_list_item_1, scoreHistory);
         listView.setAdapter(adapterList);
@@ -112,9 +119,8 @@ public class ActivityMain extends ActionBarActivity {
                 adb.setNegativeButton("No", null);
                 adb.setPositiveButton("Yes", new AlertDialog.OnClickListener() {
                     public void onClick(DialogInterface dialog, int which) {
-                        scoreHistory.remove(positionToRemove);
+                        updateScoreFromItem(scoreHistory.remove(positionToRemove));
                         adapterList.notifyDataSetChanged();
-                        updateScore();
                     }
                 });
                 adb.show();
@@ -123,10 +129,104 @@ public class ActivityMain extends ActionBarActivity {
     }
 
     public void scoreRound(View v){
+        Spinner spinnerPoints;
+        String history;
+        String myName;
+        String yourName;
+        int score;
+        String temp;
 
+        if(editMode)//if we are starting a brand new game right now
+        {
+            //disable a bunch of stuff that should not change through the course of a game
+            ((EditText) findViewById(R.id.editPlayer1)).setEnabled(false);
+            ((EditText) findViewById(R.id.editPlayer2)).setEnabled(false);
+            ((Button)findViewById(R.id.buttonSpy)).setEnabled(false);
+            editMode=false;
+        }
+
+        //get myName and yourName
+        temp=((EditText) findViewById(R.id.editPlayer1)).getText().toString();
+        if(temp.length()>0)
+            myName=temp;
+        else
+            myName="You";
+        temp=((EditText) findViewById(R.id.editPlayer2)).getText().toString();
+        if(temp.length()>0)
+            yourName=temp;
+        else
+            yourName="Opponent";
+
+        //get score
+        if(currentSpyIsMe)
+            spinnerPoints = (Spinner) findViewById(R.id.spinnerMyPoints);
+        else
+            spinnerPoints = (Spinner) findViewById(R.id.spinnerYourPoints);
+        score=spinnerPoints.getSelectedItemPosition();
+
+        //create text to add to history
+        if(currentSpyIsMe)
+        {
+            history=""+myName+" scored "+score+" points.";
+            totalScore+=score;
+        }
+        else
+        {
+            history=""+yourName+" scored "+score+" points.";
+            totalScore-=score;
+        }
+
+        history+="\nYou are "+Math.abs(totalScore)+" point";
+        if(totalScore!=1 && totalScore!=-1)
+            history+="s";
+        if (totalScore<0)
+            history+=" behind.";
+        else
+            history+=" ahead.";
+
+
+
+        if(switchThisRound)//are we supposed to switch this round
+            switchSpy();
+        else
+            switchThisRound=true;//if not, then we'll switch next round
+
+        updateScore();//totalScore has changed, display it
+        scoreHistory.add(0,history);//add history from the top
+        adapterList.notifyDataSetChanged();//update the listview
     }
 
-    public void updateScore(){//takes the list, and converts it to score
+    public void switchSpy(){
+        if(currentSpyIsMe)
+        {
+            currentSpyIsMe=false;
+        }
+        else
+        {
+            currentSpyIsMe=true;
+        }
+        switchThisRound=false;
+        notifySpinner();//change layout so the spinner (to add score) is under the current spy
+    }
 
+    public void updateScoreFromItem(String item){//takes the removed item, and parses it to determine how to update the score
+        int start=item.indexOf(" scored ")+8;//add 8 because of 8 shown chars
+        int end= item.indexOf(" points.");//end point is exclusive, and indexOf returns the start of the string, so no change
+        int scoreDiff=Integer.parseInt(item.substring(start,end));
+
+        //now, to add or subtract?
+
+        updateScore();
+    }
+
+    public void updateScore(){
+        String score="You are "+Math.abs(totalScore)+" point";
+        if(totalScore!=1 && totalScore!=-1)
+            score+="s";
+        if (totalScore<0)
+            score+=" behind.";
+        else
+            score+=" ahead.";
+        ((TextView)findViewById(R.id.textScore)).setText(score);
     }
 }
